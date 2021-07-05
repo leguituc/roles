7aefa6b4468ea27a4179ea7e04be38d192f3fb7a
leguituc <33308741+leguituc@users.noreply.github.com>
2021-07-05 16:56:32 -0300

    Update UserSeeder.php

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -17,7 +17,7 @@ class UserSeeder extends Seeder
         $admin = User::create([
             'name' => 'Luis',
             'email' => 'ingelegui@gmail.com',
-            'password' => '$2y$10$.jDlyzBcuMtHl9dkyfHVv.v4zEP0ANttAanlOpJybHDvukKSrIYz6'
+            'password' => '$2y$10$.jDlyzBcuMtHl9dkyfHVv.v4zEP0ANttAanlOpJybHDvukKSrIYz6' //arz48tuc
         ])->assignRole('admin');
         $user = User::create([
             'name' => 'Pedro',
